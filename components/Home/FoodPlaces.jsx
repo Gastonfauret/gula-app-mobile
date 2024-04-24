728fa5267461533f087f4eb586abd83d9ba7992c
Gastonfauret <Gastonfauret@gmail.com>
2024-04-24 10:39:43 -0300

    three lines of banner. Done!

--- components/Home/FoodPlaces.jsx
+++ components/Home/FoodPlaces.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
-import { View, StyleSheet, Text, ScrollView } from 'react-native'
+import { View, StyleSheet, Text, ScrollView, Image } from 'react-native'
 
 export default function FoodPlaces() {
     return (
         <View style={styles.container}>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                 <View style={styles.subContainer}>
-                    <Text>Rotiseria Pablo</Text>
+                    <Image source={require('../../assets/Food Places/roti-pablo.jpg')} style={styles.fondoLocal} />
+                    <Text style={styles.textoLocales}>Rotiseria Pablo</Text>
                 </View>
                 <View style={styles.subContainer}>
-                    <Text>Lo de Chairo</Text>
+                    <Image source={require('../../assets/Food Places/chairo.jpg')} style={styles.fondoLocal} />
+                    <Text style={styles.textoLocales}>Lo de Chairo</Text>
                 </View>
                 <View style={styles.subContainer}>
-                    <Text>Aka Pizzas</Text>
+                    <Image source={require('../../assets/Food Places/ak-pizzas.jpg')} style={styles.fondoLocal} />
+                    <Text style={styles.textoLocales}>Aka Pizzas</Text>
                 </View>
                 <View style={styles.subContainer}>
-                    <Text>Cerveceria Jooks</Text>
+                    <Image source={require('../../assets/Food Places/jooks.jpg')} style={styles.fondoLocal} />
+                    <Text style={styles.textoLocales}>Cerveceria Jooks</Text>
                 </View>
                 <View style={styles.subContainer}>
-                    <Text>La Paperia</Text>
+                    <Image source={require('../../assets/Food Places/paperia.jpeg')} style={styles.fondoLocal} />
+                    <Text style={styles.textoLocales}>La Paperia</Text>
                 </View>
                 <View style={styles.subContainer}>
-                    <Text>Sabores al Paso</Text>
+                    <Image source={require('../../assets/Food Places/sabores-al-paso.jpg')} style={styles.fondoLocal} />
+                    <Text style={styles.textoLocales}>Sabores al Paso</Text>                    
                 </View>
             </ScrollView>
         </View>
@@ -29,22 +35,49 @@ export default function FoodPlaces() {
 }
 
 const styles = StyleSheet.create({
-    container: {        
+    container: {
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
         width: '100%',
-        height: 150,        
+        height: 150,
         marginVertical: '2%'
     },
 
     subContainer: {
-        alignItems: 'center',     
-        justifyContent: 'center',         
+        alignItems: 'center',
+        justifyContent: 'center',
         width: 160,
         height: 145,
         marginHorizontal: 5,
         backgroundColor: '#d3bcf6',
         borderRadius: 5
+    },
+
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.9)',
+    },
+
+    fondoLocal: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        borderRadius: 5
+    },
+
+    textoLocales: {               
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+        position: 'absolute',       
+        borderRadius: 5,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        textAlignVertical: 'center'      
     }
 })
